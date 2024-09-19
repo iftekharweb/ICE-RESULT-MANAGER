a8871eec6dc5b408b2a4fbd5f2e323a0cf67b63d
Iftekhar Md. Shishir <iftekharweb@gmail.com>
2024-09-19 22:14:58 +0600

    Loader added for eatch page

---
+++ next-frontend/src/app/user/profile/loading.js
@@ -0,0 +1,14 @@
+import LoadingSVG from "@/public/Infinity.svg";
+import Image from "next/image";
+
+const Loading = () => {
+  return (
+    <div className="h-[90vh] w-full flex justify-center items-center">
+      <div className="m-2">
+        <Image src={LoadingSVG} width={200} height={100} />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
